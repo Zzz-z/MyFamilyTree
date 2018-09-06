516b77cfc1d647544350be083306f168f6b699ac
Zzz-z <antongrebennikov1@gmail.com>
2018-09-07 02:40:14 +0300

    Spring MVC, Spring Security

---
+++ src/main/java/com/miracle/tree/Start.java
@@ -0,0 +1,13 @@
+package com.miracle.tree;
+
+import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Start {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Start.class, args);
+    }
+
+}
